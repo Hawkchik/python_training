f93eff3d8424c056ef36b33557dd54241db5a02a
Ivan <vakionika@gmail.com>
2017-09-25 18:08:31 +0300

    Рефакторинг. Добавление фисктуры.

--- test_add_group.py
+++ test_add_group.py
@@ -1,36 +1,24 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+import pytest
 from python_training.group import Group
 from python_training.application import Application
 
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
-class test_add_group(unittest.TestCase):
-    def setUp(self):
-        self.app=Application()
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
 
-    def test_test_add_group(self):
-        wd = self.wd
-        self.app.open_home_page()
-        self.app.login(username="admin", password="secret")
-        self.app.open_group_page()
-        self.app.create_group( Group(name="1", header="1",footer= "1"))
-        self.app.return_group_page()
-        self.app.logout()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
 
-    def tearDown(self):
-        self.app.destroy()
+def test_test_add_group(app):
+        app.open_home_page()
+        app.login(username="admin", password="secret")
+        app.open_group_page()
+        app.create_group( Group(name="1", header="1",footer= "1"))
+        app.return_group_page()
+        app.logout()
 
-if __name__ == '__main__':
-    unittest.main()
