829073db10140983b8ce0ef35d3b4c5de4e74e93
Ivan <vakionika@gmail.com>
2017-09-28 11:49:50 +0300

    Выделили вспомогательный класс Session

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -15,9 +15,9 @@ def app(request):
 
 def test_test_add_group(app):
         app.open_home_page()
-        app.login(username="admin", password="secret")
+        app.session.login(username="admin", password="secret")
         app.open_group_page()
         app.create_group( Group(name="1", header="1",footer= "1"))
         app.return_group_page()
-        app.logout()
+        app.session.logout()
 
