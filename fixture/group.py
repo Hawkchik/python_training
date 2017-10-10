2a5b99dad04dfe79b4acd73dcfbe1523ca5ff930
Ivan <vakionika@gmail.com>
2017-10-10 11:06:52 +0300

    Новый тест и декомпозиция!
    Ура ура

--- fixture/group.py
+++ fixture/group.py
@@ -1,13 +1,19 @@
 class GroupHelper:
     def __init__(self, app):
         self.app = app
-    def return_group_page(self):
+    def open_groups_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("groups").click()
+
+
+    def return_to_groups_page(self):
             wd = self.app.wd
             wd.find_element_by_link_text("group page").click()
             wd.find_element_by_id("logo").click()
 
     def create(self, group):
         wd = self.app.wd
+        self.open_groups_page()
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group page
@@ -22,6 +28,16 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+    # select first group
+    # delete first group
 
     def open_group_page(self):
         # open group page
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,21 +1,13 @@
 # -*- coding: utf-8 -*-
-import pytest
 
-from python_training.fixture.application import Application
 from python_training.model.group import Group
 
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
+
 
 def test_add_group(app):
-        app.open_home_page()
         app.session.login(username="admin", password="secret")
-        app.group.open_group_page()
+        app.group.open_groups_page()
         app.group.create(Group(name="1", header="1", footer="1"))
-        app.group.return_group_page()
         app.session.logout()
 
